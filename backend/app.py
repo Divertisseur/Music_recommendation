a454ba2824a047a24f17dfd592f90985400811e2
unknown <mael.korchef@gmail.com>
2025-07-15 10:50:18 +0200

    changed port

--- backend/app.py
+++ backend/app.py
@@ -145,4 +145,5 @@ def serve_music(filename):
     return send_from_directory('music_db', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port) 
